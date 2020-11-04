687324c867a0b89802fb81ee0d1184186a133469
Edward Cruz <sagecreations.ec@gmail.com>
2020-11-04 15:56:50 -0600

    added files

---
+++ StarterFiles/graphTraversalTester.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include "graphType.h"
+
+using namespace std;
+
+int main() {
+  
+  // todo: output results of depth first search here
+  // todo: output results of breadth first search here
+
+
+  return 0;
+}
